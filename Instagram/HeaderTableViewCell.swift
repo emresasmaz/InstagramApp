b8abe68b3dd9da9e189bae4c6a9a1464e69cdb41
Emre Sasmaz <sasmazemre7@gmail.com>
2016-05-25 18:00:21 +0300

    HeaderTableViewCell and HeaderTableViewCell.xib added

---
+++ Instagram/HeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeaderTableViewCell.swift
+//  Instagram
+//
+//  Created by Emre Sasmaz on 5/25/16.
+//  Copyright © 2016 Emre Sasmaz. All rights reserved.
+//
+
+import UIKit
+
+class HeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
