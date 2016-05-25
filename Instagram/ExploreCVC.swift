44e919aa6b445c484c2d85c880344a2229217511
Emre Sasmaz <sasmazemre7@gmail.com>
2016-05-25 16:55:47 +0300

    Instagram App
    View Controller deleted. ExploreCVC added for Explore collection view.
    Also ExploreCollectionViewCell added.

---
+++ Instagram/ExploreCVC.swift
@@ -0,0 +1,13 @@
+//
+//  ExploreCVC.swift
+//  Instagram
+//
+//  Created by Emre Sasmaz on 5/25/16.
+//  Copyright © 2016 Emre Sasmaz. All rights reserved.
+//
+
+import UIKit
+
+class ExploreCVC: UICollectionViewController {
+  
+}
---
+++ Instagram/ExploreCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ExploreCollectionViewCell.swift
+//  Instagram
+//
+//  Created by Emre Sasmaz on 5/25/16.
+//  Copyright © 2016 Emre Sasmaz. All rights reserved.
+//
+
+import UIKit
+
+class ExploreCollectionViewCell: UICollectionViewCell {
+
+}
